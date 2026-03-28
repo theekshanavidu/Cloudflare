8ad0cc3a557b01fde92ad00c9478e2d1f996d634
theekshanavidu <151367679+theekshanavidu@users.noreply.github.com>
2026-03-29 00:12:10 +0530

    Update

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-    // Set base to repository name for GitHub Pages
-    base: '/Study/',
-    build: {
-        outDir: 'dist',
-    }
-});
